a9b78d9d8404c4612a1a9cdd1ac202232022f92d
Carlos <carlosempd@gmail.com>
2023-07-18 10:11:42 -0400

    :wrench: chore: configure firestore module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { FirestoreModule } from './firestore/firestore.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    FirestoreModule.forRoot({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        keyFilename: configService.get<string>('SERVICE_ACCOUNT_KEY_FILE'),
+      }),
+      inject: [ConfigService]
+    }),
+    TasksModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
