50a1c78d0a41013cbb038c31a36c7c93cf6e02b4
Carlos <carlosempd@gmail.com>
2023-07-18 12:10:23 -0400

    :memo: chore: Create all documentation for endpoints using swagger

--- src/core/dto/task.dto.ts
+++ src/core/dto/task.dto.ts
@@ -1,19 +1,24 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsOptional, IsString } from "class-validator";
 
 export class TaskDto {
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     title: string;
 
+    @ApiProperty()
     @IsString()
     description?: string;
 }
 
 export class UpdateTaskDto {
+    @ApiProperty()
     @IsString()
     @IsOptional()
     title: string;
 
+    @ApiProperty()
     @IsString()
     @IsOptional()
     description?: string;
--- src/tasks/tasks.document.ts
+++ src/tasks/tasks.document.ts
@@ -1,9 +1,15 @@
+import { ApiProperty } from "@nestjs/swagger";
 
 export class TaskDocument {
     // static collectionName = configService.get<string>('DB_NAME')
     static collectionName = 'tasks';
     
+    @ApiProperty()
     id?: string;
+
+    @ApiProperty()
     title: string;
+
+    @ApiProperty()
     description: string;
 }
