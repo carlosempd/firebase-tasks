28892a2892f5e5e2a4eb6047383ae1e1676143df
Carlos <carlosempd@gmail.com>
2023-07-18 10:13:02 -0400

    :construction: feat: create endpoints and service to manage crud operations of tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { TasksService } from 'src/tasks/tasks.service';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private taskService: TasksService) {}
 
+    @Get(':id')
+    findById(@Param('id') id: string) {
+        return this.taskService.findById(id);
+    }
+
     @Get()
     findAll() {
         return this.taskService.findAll()
+    }
+
+    @Post()
+    create() {
+        return this.taskService.create({title: 'Segunda tarea', description: 'Esta es la primera tarea'});
+    }
+
+    @Put()
+    update() {
+        // return this.taskService.update()
     }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,12 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { MockService } from '../core/services/mock.service';
+import { TaskDocument } from './tasks.document';
+import { CollectionReference } from '@google-cloud/firestore';
 
 @Injectable()
 export class TasksService {
-    constructor (private mockService: MockService) {}
+    constructor (
+        private mockService: MockService,
+        @Inject(TaskDocument.collectionName)
+        private tasksCollection: CollectionReference<TaskDocument>
+    ) {}
 
-    findAll() {
-        return this.mockService.getTasks();
+    async findById(id: string): Promise<TaskDocument> {
+        const docReference = this.tasksCollection.doc(id);
+        const taskDocument = await docReference.get();
+        if (!taskDocument.exists){
+            throw new BadRequestException({
+                message: 'Not found'
+            });
+        }
+        return taskDocument.data();
+    }
+
+    async findAll(): Promise<TaskDocument[]> {
+        const snapshot = await this.tasksCollection.get();
+        const tasks: TaskDocument[] = [];
+        snapshot.forEach(doc => {
+            const task = doc.data();
+            task.id = doc.id;
+            tasks.push(task)
+        })
+        return tasks;
+    }
+
+    async create(data: { title: string, description: string }): Promise<TaskDocument> {
+        // const docReference = this.tasksCollection.doc(data.title);
+        // await docReference.set({ ...data });
+        // const taskDocument = await docReference.get();
+        // const task = taskDocument.data();
+        // return task
+        const docReference = await this.tasksCollection.add({...data});
+        const taskDocument = await docReference.get();
+        return { id: taskDocument.id, ...taskDocument.data() };
+    }
+
+    async update(id: string, data): Promise<TaskDocument> {
+        const docReference = this.tasksCollection.doc(id);
+        await docReference.update(data);
+        const taskDocument = await docReference.get();
+        return taskDocument.data();
     }
     
 }
